a83c3c1d8739307923b0b7ac0b411ee091abd825
Adridri7 <adrieen.rocchetti@gmail.com>
2024-12-17 10:41:14 +0100

    init html css & js

---
+++ index.js
@@ -0,0 +1,9 @@
+const test = document.createElement("div"); 
+test.style.backgroundColor = "#fff";
+test.style.width = '10px';   
+test.style.height = '10px';  
+
+const game_container = document.getElementById("game-container")
+
+game_container.appendChild(test)
+
